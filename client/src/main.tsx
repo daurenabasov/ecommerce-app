01b82e4e0973140ea1d431bc69c417e4c70ac809
daurenabasov <daurenabasov987@gmail.com>
2023-05-30 20:06:21 +0600

    add new types

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,13 @@
-import ReactDOM from 'react-dom/client';
-import App from './app/App';
-import './styles/index.scss';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { setupStore } from './redux/store';
+import ReactDOM from "react-dom/client";
+import App from "./app/App";
+import "./styles/index.scss";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { setupStore } from "./redux/store";
 
 const store = setupStore();
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
     <Provider store={store}>
       <App />
